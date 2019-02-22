147e3b0125de4e728f17119e2531400391b5da45
EricHendraJr <erichendra94@gmail.com>
2019-02-22 08:48:28 +0700

    New CRUD Tunjangan Profesi & Tugas Belajar

--- application/modules/master/controllers/Tunjangan_Profesi.php
+++ application/modules/master/controllers/Tunjangan_Profesi.php
@@ -8,7 +8,7 @@ class Tunjangan_profesi extends CI_Controller {
 		$this->load->model ('Mmaster', '', TRUE);
 	}
     
-    public function tunjangan_profesi()
+    public function index()
 	{
 		# code...
 		$this->Globalrules->session_rule();
@@ -16,6 +16,96 @@ class Tunjangan_profesi extends CI_Controller {
 		$data['content'] = 'master/tunjangan_profesi/data_tunjangan_profesi';
 		$data['list']    = $this->Mmaster->get_data_tunjangan_profesi();
 		$this->load->view('templateAdmin',$data);
+	}
+
+	public function store($arg=NULL,$oid=NULL)
+	{
+		# code...
+		$res_data    = 0;
+		$text_status = '';
+		$data_sender = array();
+		if ($arg == NULL) {
+			# code...
+			$data_sender = $this->input->post('data_sender');
+		}
+		else {
+			# code...
+			$data_sender['crud'] = $arg;
+			$data_sender['oid']  = $oid;
+		}
+
+		
+			// $data_store        = $this->Globalrules->trigger_insert_update($data_sender['crud']);
+			if ($data_sender['crud'] == 'insert') {
+				# code...
+				$check_nip = $this->Mmaster->get_data_pegawai_nip($data_sender['nip']);
+				if ($check_nip != 0) {
+					$get_tunjangan_profesi_pegawai = $this->Mmaster->get_tunjangan_profesi_pegawai($check_nip[0]->id,'DESC');
+					if ($get_tunjangan_profesi_pegawai != 0) {
+						if ($get_tunjangan_profesi_pegawai[0]->tunjangan == $data_sender['tunjangan'] || $get_tunjangan_profesi_pegawai[0]->tgl_mulai >= date('Y-m-d',strtotime($data_sender['tgl_mulai']))) {
+							$res_data       = '2';
+							$text_status    = $this->Globalrules->check_status_res($res_data,'Tunjangan sama dengan tunjangan sebelumnya.');
+						}
+						else {
+							$data_last = array
+							(
+								'tgl_selesai' => date('Y-m-d',strtotime($data_sender['tgl_mulai'] . "-1 days"))
+							);
+							$flag = array
+							(
+								'id' => $get_tunjangan_profesi_pegawai[0]->id
+							);
+							$this->Allcrud->editData('mr_tunjangan_profesi',$data_last,$flag);
+
+							$data_store['id_pegawai'] = $check_nip[0]->id;
+							$data_store['tgl_mulai'] = date('Y-m-d',strtotime($data_sender['tgl_mulai']));
+							$data_store['tgl_selesai'] = '9999-01-01';
+							$data_store['tunjangan'] = $data_sender['tunjangan'];
+							$res_data       = $this->Allcrud->addData('mr_tunjangan_profesi',$data_store);
+							$text_status    = $this->Globalrules->check_status_res($res_data,'Data Tunjangan Profesi telah berhasil ditambahkan.');
+						}
+					}
+					else {
+						$data_store['id_pegawai'] = $check_nip[0]->id;
+						$data_store['tgl_mulai'] = date('Y-m-d',strtotime($data_sender['tgl_mulai']));
+						$data_store['tgl_selesai'] = '9999-01-01';
+						$data_store['tunjangan'] = $data_sender['tunjangan'];
+						$res_data       = $this->Allcrud->addData('mr_tunjangan_profesi',$data_store);
+						$text_status    = $this->Globalrules->check_status_res($res_data,'Data Tunjangan Profesi telah berhasil ditambahkan.');
+					}
+				}
+			} elseif ($data_sender['crud'] == 'update') {
+				# code...	
+				$check_nip = $this->Mmaster->get_data_pegawai_nip($data_sender['nip']);		
+				$data_store['id_pegawai'] = $check_nip[0]->id;
+				$data_store['tgl_mulai'] = date('Y-m-d',strtotime($data_sender['tgl_mulai']));
+				$data_store['tgl_selesai'] = '9999-01-01';
+				$data_store['tunjangan'] = $data_sender['tunjangan'];
+				$res_data       = $this->Allcrud->editData('mr_tunjangan_profesi',$data_store,array('id'=>$data_sender['oid']));
+				$text_status    = $this->Globalrules->check_status_res($res_data,'Data Tunjangan Profesi telah berhasil diubah.');
+			} elseif ($data_sender['crud'] == 'delete') {
+				# code...
+				$res_data    = $this->Allcrud->delData('mr_tunjangan_profesi',array('id'=>$data_sender['oid']));
+				$text_status = $this->Globalrules->check_status_res($res_data,'Data Tunjangan Profesi telah berhasil dihapus.');
+			}
+
+		$res = array
+					(
+						'status' => $res_data,
+						'text'   => $text_status
+					);
+		echo json_encode($res);		
+	}
+
+	public function get_data_tunjangan_profesi($id)
+	{
+		$this->Globalrules->session_rule();						
+		$flag = array('id'=>$id);
+		$q    = $this->Allcrud->getData('mr_tunjangan_profesi',$flag)->result_array();
+		$pegawai = $this->Mmaster->get_data_pegawai($q[0]['id_pegawai']);
+		$q[0]['nip'] = $pegawai[0]->nip;
+		// print_r($q[0]['nip']);die();
+		echo json_encode($q);
 	}
 
 	public function edit_tunjangan_profesi($id)
