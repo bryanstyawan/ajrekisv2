655e4513bf340421669b0390a1a27f06b0278bfb
bryanstyawan <bryanstyawan@hotmail.com>
2019-03-06 14:03:41 +0700

    hapus target skp yang bersumber dari skp pribadi

--- application/modules/skp/views/skp_pegawai.php
+++ application/modules/skp/views/skp_pegawai.php
@@ -334,22 +334,33 @@
                         <td style="<?=$style_td;?>">
                             <button class="btn btn-warning btn-xs" onclick="edit('<?=$list[$i]->skp_id;?>','<?=$list[$i]->status;?>','<?=$list[$i]->edit_status;?>')"><i class="fa fa-edit"></i>&nbsp;Ubah Target</button>
                             &nbsp;
-                            <?php 
-                            if ($list[$i]->PK == 1) {
-                                # code...
-                                if ($who_is == 'eselon 2') {
+                            <?php
+                                if ($list[$i]->id_skp_master == NULL || $list[$i]->id_skp_jfu == NULL || $list[$i]->id_skp_jft == NULL) {
                                     # code...
+                                    if ($list[$i]->status != 1) {
+                                        # code...
+                            ?>
+                                        <button class="btn btn-danger btn-xs" style="margin-top:10px;" onclick="del('<?=$list[$i]->skp_id;?>')"><i class="fa fa-edit"></i>&nbsp;Hapus Target</button>                            
+                            <?                                        
+                                    }
+                                }
+                            ?>
+                            <?php 
+                            // if ($list[$i]->PK == 1) {
+                            //     # code...
+                            //     if ($who_is == 'eselon 2') {
+                            //         # code...
                             ?>
                                     <!-- <button class="btn btn-warning btn-xs" style="margin-top:5px;" onclick="kegiatan_es2('<?=$list[$i]->skp_id;?>')"><i class="fa fa-edit"></i>&nbsp;Kegiatan Eselon 3</button> -->
                             <?php
-                                }
-                                elseif ($who_is == 'eselon 3') {
-                                    # code...
+                            //     }
+                            //     elseif ($who_is == 'eselon 3') {
+                            //         # code...
                             ?>
-                                    <!-- <button class="btn btn-warning btn-xs" style="margin-top:5px;" onclick="kegiatan_es3('<?=$list[$i]->skp_id;?>')"><i class="fa fa-edit"></i>&nbsp;Kegiatan Eselon 4</button> -->
+                                     <!-- <button class="btn btn-warning btn-xs" style="margin-top:5px;" onclick="kegiatan_es3('<?=$list[$i]->skp_id;?>')"><i class="fa fa-edit"></i>&nbsp;Kegiatan Eselon 4</button> -->
                             <?php                                    
-                                }                                
-                            }
+                            //     }                                
+                            // }
                             ?>                            
                         </td>
                     </tr>
