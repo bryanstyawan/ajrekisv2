c6f01e8afdcbb429efa49af64de2b652337e267b
bryanstyawan <bryanstyawan@hotmail.com>
2019-04-10 10:20:58 +0700

    export dan olah data kinerja

--- application/modules/laporan/controllers/Laporan.php
+++ application/modules/laporan/controllers/Laporan.php
@@ -249,10 +249,10 @@ class Laporan extends CI_Controller {
 		$bulan       = '0'.$data_sender['data_5'];
 		if ($data_sender['data_5'] == 3) {
 			# code...
-			$bulan = '01';
+			$bulan = '1';
 		}
 		$tahun       = $data_sender['data_6'];
-		$data_date   = $tahun.'-'.$bulan;		
+		$data_date   = $tahun.'-0'.$bulan;		
 		$data_sender = array
 						(
 							'eselon1'    => $data_sender['data_1'],
@@ -272,7 +272,7 @@ class Laporan extends CI_Controller {
 		if ($data['list'] != 0) {
 			# code...
 			for ($i=0; $i < count($data['list']); $i++) { 
-				$this->Globalrules->sync_data_transaction(array('status_pekerjaan'=>1,'id_pegawai'=>$this->session->userdata('sesUser'),'tanggal_mulai LIKE'=>$data_date.'%'),$bulan,$tahun);				
+				$this->Globalrules->sync_data_transaction(array('status_pekerjaan'=>1,'id_pegawai'=>$data['list'][$i]->id_pegawai,'tanggal_mulai LIKE'=>$data_date.'%'),$bulan,$tahun);				
 			}
 		}		
 	}
