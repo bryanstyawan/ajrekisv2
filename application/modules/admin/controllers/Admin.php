6d83f0a3af6482de868a2ad78223a2241c8c692c
bryanstyawan <bryanstyawan@hotmail.com>
2019-07-10 16:14:28 +0700

    merge and fix conflict from dev

--- application/modules/admin/controllers/Admin.php
+++ application/modules/admin/controllers/Admin.php
@@ -22,10 +22,21 @@ class Admin extends CI_Controller
 		# code...
 		$this->Globalrules->session_rule();
 		$this->Globalrules->notif_message();		
-		$data['title']      = 'Ubah Password';		
-		$data['subtitle']   = '';
-		$data['content']    = 'admin/user/change_password';
-		$this->load->view('templateAdmin',$data);		
+		$data['title']      		= 'Ubah Password';		
+		$data['subtitle']   		= '';
+		$data['content']    		= 'admin/user/change_password';
+		$data['infoPegawai']        = $this->Globalrules->get_info_pegawai();
+		$data['agama']              = $this->Allcrud->listData('mr_agama')->result_array();
+		$data['golongan']           = $this->Allcrud->listData('mr_golongan')->result_array();
+		// $data['info_kompetensi']    = $this->Allcrud->getData('mr_kompetensi',array('id_pegawai'=>$this->session->userdata('sesUser')))->result_array();
+		$data['history_golongan']   = $this->Globalrules->get_history_golongan();
+		$data['history_jabatan']  	= $this->Globalrules->get_history_jabatan();
+		$data['history_pendidikan'] = $this->Globalrules->get_history_pendidikan();
+		$data['diklat_struktural'] 	= $this->Globalrules->get_history_diklat('1');
+		$data['diklat_fungsional'] 	= $this->Globalrules->get_history_diklat('2');
+		$data['diklat_teknis'] 		= $this->Globalrules->get_history_diklat('3');
+		$data['pendidikan'] 		= $this->Allcrud->listData('mr_pendidikan');
+		$data['diklat'] 			= $this->Allcrud->listData('mr_diklat');		$this->load->view('templateAdmin',$data);		
 	}
 
 	public function do_change_password()
@@ -397,15 +408,6 @@ class Admin extends CI_Controller
 		$this->load->view('akses/ajaxAkses',$data);
 	}
 	
-
-
-
-
-
-
-
-
-
 	public function caries2()
 	{
 		$this->Globalrules->notif_message();				
@@ -487,4 +489,121 @@ class Admin extends CI_Controller
 	// 	}
 	// 	redirect('admin/urtug');
 	// }
+
+	public function get_data_pendidikan($id_pegawai){
+		$this->Globalrules->session_rule();
+		$data['pendidikan'] = $this->Allcrud->getData('mr_history_pendidikan',array('id'=>$id_pegawai))->result_array();
+		if ($data['pendidikan'] != array()) {
+			$data['list_pendidikan']      = $this->Allcrud->getData('mr_pendidikan',array('id_pendidikan'=>$data['pendidikan'][0]['id_pendidikan']))->result_array();
+		}
+		echo json_encode($data);
+	}
+
+	public function get_data_diklat($id_pegawai){
+		$this->Globalrules->session_rule();
+		$data['diklat'] = $this->Allcrud->getData('mr_history_diklat',array('id'=>$id_pegawai))->result_array();
+		if ($data['diklat'] != array()) {
+			$data['list_diklat']      = $this->Allcrud->getData('mr_diklat',array('id_diklat'=>$data['diklat'][0]['id_diklat']))->result_array();
+		}
+		echo json_encode($data);
+	}
+	
+	public function store_pendidikan($arg=NULL,$oid=NULL)
+	{
+		$res_data    = 0;
+		$text_status = "";
+		$data_sender = array();
+		if ($arg == NULL) {
+			$data_sender = $this->input->post('data_sender');
+		}
+		else {
+			$data_sender['crud'] = $arg;
+			$data_sender['oid']  = $oid;
+		}
+		
+		// print_r($data_sender['crud']);die();
+		if ($data_sender['crud'] != 'delete') {
+			$data_store['id_pegawai'] 		= $this->session->userdata('sesUser');
+			$data_store['id_pendidikan'] 	= $data_sender['id_pendidikan'];
+			$data_store['jurusan'] 			= $data_sender['jurusan'];
+			$data_store['nama_sekolah'] 	= $data_sender['nama_sekolah'];
+			$data_store['lokasi_sekolah'] 	= $data_sender['lokasi_sekolah'];
+			$data_store['tahun_lulus'] 		= $data_sender['tahun_lulus'];
+			$data_store['audit_time'] 		= date('Y-m-d H:i:s');
+			$data_store['audit_user'] 		= $this->session->userdata('sesUser');
+
+			if ($data_sender['crud'] == 'insert') {
+				$res_data       = $this->Allcrud->addData('mr_history_pendidikan',$data_store);
+				$text_status 	= $this->Globalrules->check_status_res($res_data,'Data Pendidikan telah berhasil ditambah.');
+			}
+			elseif ($data_sender['crud'] == 'update') {
+				$res_data    = $this->Allcrud->editData('mr_history_pendidikan',$data_store,array('id'=>$data_sender['oid']));
+				$text_status = $this->Globalrules->check_status_res($res_data,'Data Pendidikan telah berhasil diubah.');
+			}								
+		}
+		else {
+			if ($data_sender['crud'] == 'delete') {
+				
+				$res_data    = $this->Allcrud->delData('mr_history_pendidikan',array('id' => $data_sender['oid']));				
+				$text_status = $this->Globalrules->check_status_res($res_data,'Data Pendidikan telah berhasil dihapus.');											
+			}				
+		}
+		$res = array
+				(
+					'status' => $res_data,
+					'text'   => $text_status
+				);
+		// print_r($res);die();
+		echo json_encode($res);											
+	}
+
+	public function store_diklat($arg=NULL,$oid=NULL)
+	{
+		$res_data    = 0;
+		$text_status = "";
+		$data_sender = array();
+		if ($arg == NULL) {
+			$data_sender = $this->input->post('data_sender');
+		}
+		else {
+			$data_sender['crud'] = $arg;
+			$data_sender['oid']  = $oid;
+		}
+		
+		// print_r($data_sender['crud']);die();
+		if ($data_sender['crud'] != 'delete') {
+			$data_store['id_pegawai'] 		= $this->session->userdata('sesUser');
+			$data_store['id_diklat'] 		= $data_sender['id_diklat'];
+			$data_store['nama_diklat'] 		= $data_sender['nama_diklat'];
+			$data_store['tempat'] 			= $data_sender['tempat'];
+			$data_store['panitia'] 			= $data_sender['panitia'];
+			$data_store['tgl_mulai'] 		= $data_sender['tgl_mulai'];
+			$data_store['tgl_selesai'] 		= $data_sender['tgl_selesai'];
+			$data_store['audit_time'] 		= date('Y-m-d H:i:s');
+			$data_store['audit_user'] 		= $this->session->userdata('sesUser');
+
+			if ($data_sender['crud'] == 'insert') {
+				$res_data       = $this->Allcrud->addData('mr_history_diklat',$data_store);
+				$text_status 	= $this->Globalrules->check_status_res($res_data,'Data Diklat telah berhasil ditambah.');
+			}
+			elseif ($data_sender['crud'] == 'update') {
+				$res_data    = $this->Allcrud->editData('mr_history_diklat',$data_store,array('id'=>$data_sender['oid']));
+				$text_status = $this->Globalrules->check_status_res($res_data,'Data Diklat telah berhasil diubah.');
+			}								
+		}
+		else {
+			if ($data_sender['crud'] == 'delete') {
+				
+				$res_data    = $this->Allcrud->delData('mr_history_diklat',array('id' => $data_sender['oid']));				
+				$text_status = $this->Globalrules->check_status_res($res_data,'Data Diklat telah berhasil dihapus.');											
+			}				
+		}
+		$res = array
+				(
+					'status' => $res_data,
+					'text'   => $text_status
+				);
+		// print_r($res);die();
+		echo json_encode($res);											
+	}	
 }
