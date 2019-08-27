81a1a5a2458e83283237d362f76431148dd9e7c5
EricHendraJr <erichendra94@gmail.com>
2019-08-27 17:00:42 +0700

    Fix Approval SKP dan Kinerja Anggota Akademik

--- application/modules/laporan/controllers/Laporan.php
+++ application/modules/laporan/controllers/Laporan.php
@@ -414,6 +414,60 @@ class Laporan extends CI_Controller {
 		}
 	}
 
+	public function kinerja_anggota_akademik()
+	{
+		# code...
+		$data['title']      = 'Rekapitulasi Data Kinerja Anggota Struktur Akademik';
+		$data['content']    = 'laporan/kinerja/data_kinerja_anggota_akademik';
+		$this->load->view('templateAdmin',$data);
+		// print_r($data['list']);die();
+	}
+
+	Public function filter_kinerja_anggota_akademik()
+	{
+		$get_data_pegawai = $this->Allcrud->getData('mr_pegawai',array('id'=>$this->session->userdata('sesUser')))->result_array();
+		// $id = $this->session->userdata('sesPosisi');\
+		$id = $get_data_pegawai[0]['posisi_akademik'];
+		$data_sender = $this->input->post('data_sender');	
+		$data_sender = array
+						(
+							'atasan'   => $id,
+							'bulan'    => $data_sender['data_5'],
+							'tahun'    => $data_sender['data_6']
+						);
+		$data['sender'] 	= $data_sender;
+		$data['list'] 		= $this->Mmaster->list_bawahan($data_sender);
+		if ($data['list'] != 0) 
+		{
+			for ($i=0; $i < count($data['list']); $i++)
+			{
+				
+				$data_rekap = $this->Mmaster->list_kinerja($data['list'][$i]->id,$data['list'][$i]->posisi,$data_sender);
+				// print_r($data_rekap);die();
+				if ($data_rekap != 0) 
+				{
+					# code..
+					$data['list'][$i]->tr_belum_diperiksa   		= $data_rekap[0]->tr_belum_diperiksa;
+					$data['list'][$i]->tr_revisi       				= $data_rekap[0]->tr_revisi;
+					$data['list'][$i]->tr_tolak     				= $data_rekap[0]->tr_tolak;
+					$data['list'][$i]->tr_approve    	    		= $data_rekap[0]->tr_approve;
+					$data['list'][$i]->menit_efektif  				= $data_rekap[0]->menit_efektif;
+					$data['list'][$i]->prosentase_menit_efektif  	= $data_rekap[0]->prosentase_menit_efektif;
+				}
+				else
+				{
+					$data['list'][$i]->tr_belum_diperiksa   		= 0;
+					$data['list'][$i]->tr_revisi       				= 0;
+					$data['list'][$i]->tr_tolak     				= 0;
+					$data['list'][$i]->tr_approve    	    		= 0;
+					$data['list'][$i]->menit_efektif  				= 0;
+					$data['list'][$i]->prosentase_menit_efektif  	= 0;
+				}
+			}
+			$this->load->view('laporan/kinerja/ajax_kinerja_anggota_akademik',$data);
+		}
+	}
+
 	/***************************** */
 
 	public function rekap_tunkir_produktivitas_disiplin()
