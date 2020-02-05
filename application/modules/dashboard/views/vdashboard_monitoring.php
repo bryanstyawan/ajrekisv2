17604cd8f19fd238e8845be01ec284adceb0c430
bryanstyawan <bryanstyawan@hotmail.com>
2020-02-05 16:31:48 +0700

    dashboard bug fixing, request bug fixing

--- application/modules/dashboard/views/vdashboard_monitoring.php
+++ application/modules/dashboard/views/vdashboard_monitoring.php
@@ -1,7 +1,7 @@
 <div class="col-md-12 tour-step tour1" id="main-dashboard">
 <?php
         $this->load->view('dashboard_component/common_component',array(
-            'class'     => 'col-lg-3 col-xs-8',
+            'class'     => 'col-lg-3 col-xs-3',
             'id'        => 'btn_masih_diproses',
             'color_box' => 'background-color: #d2d6de !important;',
             'icon'      => '',
@@ -10,7 +10,7 @@
             'html'      => ''));
 
         $this->load->view('dashboard_component/common_component',array(
-            'class'     => 'col-lg-3 col-xs-8',
+            'class'     => 'col-lg-3 col-xs-3',
             'id'        => 'btn_masih_diproses',
             'color_box' => 'background-color: #d2d6de !important;',
             'icon'      => '',
@@ -19,7 +19,7 @@
             'html'      => ''));        
 
         $this->load->view('dashboard_component/common_component',array(
-            'class'     => 'col-lg-3 col-xs-8',
+            'class'     => 'col-lg-3 col-xs-3',
             'id'        => 'btn_masih_diproses',
             'color_box' => 'background-color: #d2d6de !important;',
             'icon'      => '',
@@ -28,7 +28,7 @@
             'html'      => ''));
             
             $this->load->view('dashboard_component/common_component',array(
-                'class'     => 'col-lg-3 col-xs-8',
+                'class'     => 'col-lg-3 col-xs-3',
                 'id'        => 'btn_masih_diproses',
                 'color_box' => 'background-color: #d2d6de !important;',
                 'icon'      => '',
